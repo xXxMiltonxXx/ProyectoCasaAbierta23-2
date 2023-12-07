4c9d2c37b86e6941e56968090f7900b3fa7f50c1
xXxMiltonxXx <mmoya0992@gmail.com>
2023-12-07 02:36:30 -0500

    database creation and synchronization

--- quilluleam/src/trpc/index.ts
+++ quilluleam/src/trpc/index.ts
@@ -1,15 +1,31 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/types/server";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
+import { db } from "@/db";
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
     const user = await getUser();
 
-    if (( !user?.id) || !user?.email)
+    if (!user?.id || !user?.email)
       throw new TRPCError({ code: "UNAUTHORIZED" });
-    //verificar que el usuario este en la basew de datos  
-    return{success:true}
+    //verificar que el usuario este en la base de datos
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
+
+    if (!dbUser) {
+      //se crea un usuario en la Bd
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+    }
+    return { success: true };
   }),
 });
 export type AppRouter = typeof appRouter;
