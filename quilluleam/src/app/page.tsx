a81adfafdb2a30a7863efbc8fee8da2519214e4f
xXxMiltonxXx <mmoya0992@gmail.com>
2023-12-08 07:57:06 -0500

    solo falta stripe p1

--- quilluleam/src/app/page.tsx
+++ quilluleam/src/app/page.tsx
@@ -10,11 +10,7 @@ export default function Home() {
   return (
     <>
       <MaxWidthWrapper className="mb-12 mt-20 sm:mt-40 flex flex-col items-center justify-center text-center">
-        <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
-          <p className="text-sm font-semibold text-gray-700">
-            Quill Uleam esta publicado
-          </p>
-        </div>
+        
         <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
           Chatea con tus <span className="text-blue-600">Documentos</span> en
           segundos
